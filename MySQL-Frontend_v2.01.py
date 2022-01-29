3b5cdbb5d37dfa6e38df75d369670c789e4c56b6
adiii-ui <80589366+adiii-ui@users.noreply.github.com>
2022-01-30 00:35:09 +0400

    Update MySQL-Frontend_v2.01.py

--- MySQL-Frontend_v2.01.py
+++ MySQL-Frontend_v2.01.py
@@ -1324,7 +1324,7 @@ class wel_Login_scr(QDialog):
     def helpPopup(self):
         print('User Clicked loginHelp Btn.')
         msg="""
-This program is designed to work on MySQL Server v5.1.33 installed
+This program is designed to work on MySQL Server v5.1.33 and above, installed
 NATIVELY on your computer.
 
 Default Username: root
